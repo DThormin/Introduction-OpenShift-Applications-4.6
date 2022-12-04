b690a493edbd4ade086498a3ac653e181d64b7ee
DThormin <111440730+DThormin@users.noreply.github.com>
2022-12-03 22:14:09 -0300

    redhat

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,12 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+//Para implementar o novo recurso, adicione as seguintes linhas ao app.jsarquivo:
+
+app.get('/mars', function(req, res) {
+  res.send('Olá Marte!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
